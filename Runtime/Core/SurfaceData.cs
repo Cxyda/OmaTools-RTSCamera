795dd55cdd19d192315f0a30a5bb106248145fac
Daniel Steegmüller <daniel.steegmueller@innogames.de>
2019-07-13 18:24:47 +0200

    added assembly definition file fixed project structure

---
+++ Runtime/Core/SurfaceData.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+namespace Plugins.O.M.A.Games.RTSCamera.Core
+{
+    /// <summary>
+    /// This class provides all customizable properties for the floating feature <see cref="RTSCameraComponent"/>
+    /// </summary>
+    [Serializable]
+    public struct SurfaceData
+    {
+        [Tooltip("The layer of the GameObject where the RTS camera should hover over")]
+        public LayerMask SurfaceMask;
+        [Tooltip("The height of the camera which should be maintained")]
+        public float FloatingHeight;
+    }
+}
